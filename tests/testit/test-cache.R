34010153355ced11100a3c25bab60fb7992ec7e3
atusy <30277794+atusy@users.noreply.github.com>
2019-08-28 06:56:16 +0900

    update test-chache.R

--- tests/testit/test-cache.R
+++ tests/testit/test-cache.R
@@ -1,17 +1,16 @@
 library(testit)
 
-assert(
-  'find_globals() identifies global variables',
+assert('find_globals() identifies global variables', {
   # nothing from outside environment
-  identical(find_globals('x=1'), character(0)),
+  (find_globals('x=1') %==% character(0))
   # qwer must be created outside somewhere
-  identical(find_globals('a=1; b=a; d=qwer'), 'qwer'),
-  identical(find_globals('a=function(){f=2;g}'), 'g'),
+  (find_globals('a=1; b=a; d=qwer') %==% 'qwer')
+  (find_globals('a=function(){f=2;g}') %==% 'g')
   # y was assigned locally in z, but there is another y outside from nowhere
-  identical(find_globals('z=function(){y=1};y'), 'y'),
+  (find_globals('z=function(){y=1};y') %==% 'y')
   # more complicated cases: operators, subscripts, ...
-  identical(find_globals(c('a=1%*%1%o%2 %in% d', 'b=d%%10+3%/%2-z[1:3]')), c('d', 'z'))
-)
+  (find_globals(c('a=1%*%1%o%2 %in% d', 'b=d%%10+3%/%2-z[1:3]')) %==% c('d', 'z'))
+})
 
 assert(
   'find_symbols() identifies all symbols',
@@ -29,18 +28,19 @@ knit_lazy = function(lazy = TRUE) {
     x1 == x2  # x1 should not be updated
   })
 }
-assert(
-  'cache.lazy = TRUE/FALSE works',
-  knit_lazy(TRUE), knit_lazy(FALSE)
-)
+assert('cache.lazy = TRUE/FALSE works', {
+  (knit_lazy(TRUE))
+  (knit_lazy(FALSE))
+})
 
 knit_code$set(a = 1, b = 2, c = 3)
-assert(
-  'dep_prev() sets dependencies on previous chunks',
+assert('dep_prev() sets dependencies on previous chunks', {
   # dependency is empty now
-  identical(dep_list$get(), list()),
+  (dep_list$get() %==% list())
   # b/c depend on a, c depends on b
-  identical({dep_prev(); dep_list$get()}, list(a = c('b', 'c'), b = 'c'))
-)
+  dep_prev()
+  (dep_list$get() %==% list(a = c('b', 'c'), b = 'c'))
+})
 dep_list$restore()
 knit_code$restore()
+
